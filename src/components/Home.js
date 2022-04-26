194224bea9a7bc4c16fb5b189b069e7e01dd48fe
Maduabuchi Angus Modum <66187866+AngusOnyebuchi@users.noreply.github.com>
2022-04-27 00:24:50 +0100

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
                <p className="text-[#8892b0] py-4 max-w-[700px]">Ranging from your special breakfast, lunch and dinner. We'll always give you the best - Good Food & Good Vibes!</p>
                <div>
                    <button className="group text-white border-2 px-6 py-3 my-3 flex items-center hover:bg-pink-600 hover:border-pink-600">
-                   <NavLink href="/product">View Meals</NavLink>
+                   <NavLink to="/product">View Meals</NavLink>
                    <span className="group-hover:rotate-90 duration-300"><HiArrowNarrowRight className="ml-3 "/></span> </button>
                </div>
             </div>
