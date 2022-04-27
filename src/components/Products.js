ebd18e575b9b8d9388811df5fbdc8278ccc5deac
Maduabuchi Angus Modum <66187866+AngusOnyebuchi@users.noreply.github.com>
2022-04-27 01:31:31 +0100

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -17,7 +17,7 @@ export default function Products(props) {
                             <p className="absolute bg-white text-black top-[80%] right-0 p-[3px]">{props.time} - {props.time1} min</p>
                         </div>
                         
-                        <div className="flex items-center justify-left gap-8 py-4 overflow-hidden">
+                        <div className="flex items-center justify-left gap-16 py-4 overflow-hidden">
                             <h2>{props.food}</h2>
                             <h3>Price: ${props.price}</h3>
                             <button className="bg-gray-200 text-blue-900 px-2 py-[1px] rounded-md">Order</button>
