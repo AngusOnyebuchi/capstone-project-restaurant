35570797d5160c4b7179873400dff1e1236e80a1
Maduabuchi Angus Modum <66187866+AngusOnyebuchi@users.noreply.github.com>
2022-04-27 01:45:58 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,27 +29,27 @@ export default function Contact() {
 
       <div className='flex justify-center gap-8 md:gap-24 mt-8 mb-12'>
         <a href="https://facebook.com/profile.php?id=100008620207826">
-        <FaFacebook size={30} className="cursor-pointer"/>
+        <FaFacebook size={30} className="cursor-pointer px-2"/>
         </a>
         <a href="https://instagram.com/angus_buchi">
-        <FaInstagram size={30} className="cursor-pointer"/>
+        <FaInstagram size={30} className="cursor-pointer px-2"/>
         </a>
         <a href="https://linkedin.com/in/maduabuchi-angus-modum-715528194">
-        <FaLinkedin size={30} className="cursor-pointer"/>
+        <FaLinkedin size={30} className="cursor-pointer px-2"/>
         </a>
         <a href="https://twitter.com/AngusOnyebuchi">
-        <FaTwitter size={30} className="cursor-pointer"/>
+        <FaTwitter size={30} className="cursor-pointer px-2"/>
         </a>
         <a href="https://github.com/AngusOnyebuchi">
-        <FaGithub size={30} className="cursor-pointer"/>
+        <FaGithub size={30} className="cursor-pointer px-2"/>
         </a>
         <a href="mailto:angusonyebuchi800@gmail.com">
-        <HiOutlineMail size={30} className="cursor-pointer"/>
+        <HiOutlineMail size={30} className="cursor-pointer px-2"/>
         </a>
       </div>
 
       <div className='flex items-center justify-center gap-2 text-sm pb-4'>
-        <h4>Made with</h4> <FaHeart/> <h4>by Angus - Development 2022</h4>
+        <h4>Made with</h4> <FaHeart className="px-2"/> <h4>by Angus - Development 2022</h4>
       </div>
     </div>
 
