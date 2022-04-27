7367e7fbb61d3121c570552188c279e78910d750
Maduabuchi Angus Modum <66187866+AngusOnyebuchi@users.noreply.github.com>
2022-04-27 01:56:28 +0100

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -49,7 +49,7 @@ export default function Contact() {
       </div>
 
       <div className='flex items-center justify-center gap-2 text-sm pb-4'>
-        <h4>Made with</h4> <FaHeart/> <h4>by Angus - Development 2022</h4>
+        <h4 className="mr-1>Made withe</h4> <FaHeart/> <h4 className="ml-1>by Angus - Development 2022</h4>
       </div>
     </div>
 
